59ea881b14c973aaceb2ff8266313cf98544d05f
William C. Canin <william.costa.canin@gmail.com>
2020-02-16 03:10:48 -0300

    Update - D:16022020-H:031048

--- tests/runtests.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-## Run tests at the root of the project.
-# PYTHONPATH="." python tests/test_bake_project.py
-
-## Run tests inside the 'tests' folder.
-PYTHONPATH=".." python test_bake_project.py
